b8b2dd835ef845a1a5bcfe1123f8753fec5f8696
WagnerFLL <wsf@ic.ufal.br>
2019-06-05 19:42:33 -0300

    DR two classes

---
+++ src/train.py
@@ -0,0 +1,195 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.utils import np_utils
+from keras import backend as K
+from keras.callbacks import EarlyStopping
+from keras.callbacks import TensorBoard
+import pandas as pd
+import numpy as np
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.utils import class_weight
+import os
+
+np.random.seed(1337)
+
+
+def split_data(X, y, test_data_size):
+   
+    return train_test_split(X, y, test_size=test_data_size, random_state=42)
+
+
+def reshape_data(arr, img_rows, img_cols, channels):
+   
+    return arr.reshape(arr.shape[0], img_rows, img_cols, channels)
+
+
+def cnn_model(X_train, X_test, y_train, y_test, kernel_size, nb_filters, channels, nb_epoch, batch_size, nb_classes):
+   
+    model = Sequential()
+
+
+    model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1]),
+        padding='valid',
+        strides=4,
+        input_shape=(img_rows, img_cols, channels)))
+    model.add(Activation('relu'))
+
+
+    model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+    model.add(Activation('relu'))
+
+
+    model.add(Conv2D(nb_filters, (kernel_size[0], kernel_size[1])))
+    model.add(Activation('relu'))
+
+
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+    kernel_size = (16,16)
+    model.add(Conv2D(64, (kernel_size[0], kernel_size[1])))
+    model.add(Activation('relu'))
+    # model.add(Dropout(0.2))
+
+
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+    model.add(Flatten())
+    print("Model flattened out to: ", model.output_shape)
+
+
+    model.add(Dense(128))
+    model.add(Activation('sigmoid'))
+    model.add(Dropout(0.25))
+
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+
+    model.compile(loss = 'categorical_crossentropy',
+                    optimizer='adam',
+                    metrics=['accuracy'])
+
+
+    stop = EarlyStopping(monitor='val_acc',
+                            min_delta=0.001,
+                            patience=2,
+                            verbose=0,
+                            mode='auto')
+
+
+    tensor_board = TensorBoard(log_dir='./Graph', histogram_freq=0, write_graph=True, write_images=True)
+
+
+    hist = model.fit(X_train,y_train, batch_size=batch_size, epochs=nb_epoch,
+                verbose=1,
+                validation_split=0.2,
+                class_weight=weights,
+                callbacks=[stop, tensor_board])
+
+    return model, hist
+
+
+def save_model(model, score, model_name):
+    
+
+    if score >= 0.75:
+        print("Saving Model")
+        model.save("../models/" + model_name + "_recall_" + str(round(score,4)) + ".h5")
+    else:
+        print("Model Not Saved.  Score: ", score)
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
+
+# Specify parameters before model is run.
+batch_size = 1000
+nb_classes = 2
+nb_epoch = 30
+
+img_rows, img_cols = 256, 256
+channels = 3
+nb_filters = 32
+kernel_size = (8,8)
+
+# Import data
+l = pd.read_csv("../labels/trainLabels_master_256_v2.csv")
+labels = l.head(52000)
+das = np.load("../data/X_train.npy")
+X = das[:52000]
+y = np.array([1 if l >= 1 else 0 for l in labels['level']])
+# y = np.array(labels['level'])
+
+print(labels[labels["level"]==0].shape[0])
+# Class Weights (for imbalanced classes)
+print("Computing Class Weights")
+weights = class_weight.compute_class_weight('balanced', np.unique(y), y)
+
+
+print("Splitting data into test/ train datasets")
+X_train, X_test, y_train, y_test = split_data(X, y, 0.2)
+
+
+print("Reshaping Data")
+X_train = reshape_data(X_train, img_rows, img_cols, channels)
+X_test = reshape_data(X_test, img_rows, img_cols, channels)
+
+print("X_train Shape: ", X_train.shape)
+print("X_test Shape: ", X_test.shape)
+
+
+input_shape = (img_rows, img_cols, channels)
+
+
+print("Normalizing Data")
+print("Dt " + str(X_train.dtype))
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+print("Dt " + str(X_train.dtype))
+
+y_train = np_utils.to_categorical(y_train, nb_classes)
+y_test = np_utils.to_categorical(y_test, nb_classes)
+print("y_train Shape: ", y_train.shape)
+print("y_test Shape: ", y_test.shape)
+
+
+print("Training Model")
+
+
+model, hist = cnn_model(X_train, X_test, y_train, y_test, kernel_size, nb_filters, channels, nb_epoch, batch_size, nb_classes)
+
+
+print("Predicting")
+y_pred = model.predict(X_test)
+
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+
+y_pred = [np.argmax(y) for y in y_pred]
+y_test = [np.argmax(y) for y in y_test]
+
+
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+
+
+print("Precision: ", precision)
+print("Recall: ", recall)
+
+
+save_model(model=model, score=recall, model_name="DR_Two_Classes")
+
+
+print("Completed")
